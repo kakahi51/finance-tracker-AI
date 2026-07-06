b911e4e8ad469fd842932a8593f175c27185a101
Fadly <kfadli34@gmail.com>
2026-07-06 21:48:52 +0700

    fix: allow cors for vercel

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 const authRoutes = require('./routes/auth');
 const transactionRoutes = require('./routes/transactions');
 
-app.use(cors());
+app.use(cors({origin:'*'}));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/transactions', transactionRoutes);
@@ -20,3 +20,4 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
+app.use(cors({origin: '*' }));
